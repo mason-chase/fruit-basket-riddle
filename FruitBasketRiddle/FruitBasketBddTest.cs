072d80559a6b1c0e3f578e6f7d2a06b26a9bc20e
Mason Chase <17309021+mason-chase@users.noreply.github.com>
2021-09-14 20:00:20 +0200

    Initial empty project

---
+++ FruitBasketRiddle/FruitBasketBddTest.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace FruitBasketRiddle
+{
+    public class FruitBasketBddTest
+    {
+        [Fact]
+        public void TestSuccess()
+        {
+
+        }
+
+    }
+}
